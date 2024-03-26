dffd0d14117576edf2ba3081488523aff2ba45fc
Tanany <itstanany@gmail.com>
2024-03-26 12:42:22 +0200

    timber debug tree

--- app/src/main/java/com/openmrs/openmrsandroidclient/FhirApplication.kt
+++ app/src/main/java/com/openmrs/openmrsandroidclient/FhirApplication.kt
@@ -2,8 +2,15 @@ package com.openmrs.openmrsandroidclient
 
 import android.app.Application
 import com.google.android.fhir.datacapture.DataCaptureConfig
+import timber.log.Timber
 
 class FhirApplication: Application(), DataCaptureConfig.Provider {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
     override fun getDataCaptureConfig(): DataCaptureConfig {
         TODO("Not yet implemented")
     }
