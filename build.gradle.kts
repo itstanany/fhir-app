1b660c1871573b9ba396d90c32fd68426ba53f79
Tanany <itstanany@gmail.com>
2024-03-28 12:27:58 +0200

    add safeargs jetpack library

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,21 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:6.6.0")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle.kts files
+    }
+}
